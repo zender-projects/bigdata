b062a0edc7237ac1e690d44e19f316d1f2b84900
ivan <15101535064@163.com>
2018-01-28 10:18:36 +0800

    aaa

---
+++ spark/src/main/scala/job/WordCount.scala
@@ -0,0 +1,19 @@
+package job
+
+import org.apache.spark.{SparkConf, SparkContext}
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    val inPath = args(0)
+
+    val outPath = args(1)
+    val sparkConf = new SparkConf().setAppName("SparkWordCount")
+    val sc = new SparkContext(sparkConf)
+    val rowRdd = sc.textFile(inPath)
+    rowRdd.flatMap(_.split(" "))
+          .map((_, 1))
+          .reduceByKey(_ + _)
+          .saveAsObjectFile(outPath)
+  }
+}
