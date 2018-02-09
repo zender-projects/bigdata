d31ebc04baf4095108828a8f958b5b9abbecba11
ivan <15101535064@163.com>
2018-02-09 08:42:04 +0800

    update hdfs java client

--- hadoop/src/main/java/hdfs/HDFSJavaClient.java
+++ hadoop/src/main/java/hdfs/HDFSJavaClient.java
@@ -3,10 +3,19 @@ package hdfs;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.util.ReflectionUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.URI;
 
 public class HDFSJavaClient {
@@ -42,6 +51,50 @@ public class HDFSJavaClient {
      * */
     public void uploadFromLocal(String localPath, String hdfsPath) throws Exception{
         hdfs.copyFromLocalFile(new Path(localPath), new Path(hdfsPath));
+    }
+
+
+    /**
+     * x下载
+     * @param localPath
+     * @param hdfsPath
+     * */
+    public void downLoadFromHdfs(String localPath, String hdfsPath) throws Exception {
+        //hdfs.copyToLocalFile(new Path(hdfsPath, new Path(localPath)));
+        hdfs.copyToLocalFile(new Path(hdfsPath), new Path(localPath));
+    }
+
+
+    public void downloadFormHdfsNew(String localPath, String hdfsPath) throws Exception {
+        InputStream inputStream = hdfs.open(new Path(hdfsPath));
+
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(localPath));
+
+        IOUtils.copyBytes(inputStream, out, 1024);
+
+        IOUtils.closeStream(inputStream);
+        IOUtils.closeStream(out);
+    }
+
+    /**
+     * 压缩上传
+     *
+     * */
+    public void compressUpload(String localPath, String hdfsPath) throws Exception{
+        String codecClassName = "org.apache.hadoop.io.compress.GzipCodec";
+        Class<?> codecClass = Class.forName(codecClassName);
+
+        Configuration configuration = new Configuration();
+        CompressionCodec codec = (CompressionCodec) ReflectionUtils.newInstance(codecClass, configuration);
+
+        FileOutputStream outputStream = new FileOutputStream(hdfsPath);
+        CompressionOutputStream out = codec.createOutputStream(outputStream);
+
+        FileInputStream in = new FileInputStream(localPath);
+
+        IOUtils.copyBytes(in, out, 1024);
+
+        out.finish();
     }
 
 
